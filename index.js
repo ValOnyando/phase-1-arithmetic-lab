10ec7ac14a1750bfa4a7e0b73646addc70ba343e
Vallary Awuor Onyando <valonyando@gmail.com>
2025-03-18 10:46:09 +0300

    Performs arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,30 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+multiply === 62;
+
+let random = Math.floor(Math.random()*10);
+
+let num3 = 29;
+let num4 = 5;
+let mod = num3 % num4;
+mod === 4;
+
+let max = Math.max(4, 20, 16, 8, 4);
+
+
+
+
+
+
+// function random(){
+//     return Math.random()
+// }
+function mod(num3, num4){
+    return num3%num4 === 4
+}
+// function max(){
+//     return Math.max === 20
+// }
